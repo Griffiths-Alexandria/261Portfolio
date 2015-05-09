5dd6ba0bb93879c065d75d5f6941c539c895a976
Griffiths-Alexandria <alexcgriff@gmail.com>
2015-05-09 11:56:12 -0700

    files, files, baby

---
+++ PortfolioCIT261/Portfolio261/261PortfolioAlex/modules/header.php
@@ -0,0 +1,30 @@
+<div> 
+     
+     <div>
+        
+        <nav>
+         
+            <ul>
+                <li>
+                    <a href="/index.php" title="Home Page">Portfolio Main</a>
+                </li>
+                <li><a href="#">CSS3</a>
+                   <ul>
+                       <li><a href="/topics/cssform.php" title="CSS3 Transforms">CSS3 Transforms</a></li>
+                       <li><a href="/topics/csstrans.php" title="CSS3 Transitions">CSS3 Transitions</a></li>
+                       <li><a href="/topics/cssanimations.php" title="CSS3 Animations" >CSS3 Animations</a></li>
+                   </ul>
+                </li>
+                <li><a href="/topics/jsobjects.php" title="JavaScript Objects">JavaScript Objects</a></li>
+                <li><a href="/topics/dom.php" title="DOM manipulations">DOM Manipulations</a></li>
+                <li><a href="/topics/methods.php" title="Methodologies">Methodologies</a></li>
+                <li><a href="/topics/ajax.php" title="AJAX">AJAX</a></li>
+                <li><a href="/topics/json.php" title="JSON">JSON</a></li>
+                <li><a href="/topics/localdata.php" title="Local Data Storage">Local Data Storage</a></li>
+            </ul>
+            
+        </nav>
+         
+     </div>
+    
+</div> 
