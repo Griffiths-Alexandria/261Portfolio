0eef29777bf2d821108365c36414d39115b59835
Griffiths-Alexandria <alexcgriff@gmail.com>
2015-05-21 23:53:53 -0700

    come with me...and you'll see.. a world of pure ...imANIMATION

--- PortfolioCIT261/Portfolio261/261PortfolioAlex/topics/cssanimations.php
+++ PortfolioCIT261/Portfolio261/261PortfolioAlex/topics/cssanimations.php
@@ -17,18 +17,48 @@ and open the template in the editor.
        
         
 
-            <p class="font">
+             <p id="titleanimations" class="titleanimations flipInX">
                 CSS3 Animations
+            </p>
+            
+            <p class="paragraph">
+                CSS Animations allow for smoother element and style transitions. With CSS Animations, specifically using the @keyframe property,
+                you can specify on a percentage scale, 0% - 100% each animation result. For this title, I specified at 0%, 40%, 70%, and 100% the perspective, rotation, and background-color.<br><br>
+                There are 9 animation properties. These include: animation-delay, animation-direction, animation duration, 
+                animation-fill-mode, animation-iteration-count, animation-name, animation-play-state, animation-timing-function, 
+                and the shorthand property of animation. <br>
+                <br>
+                The page title uses the animation-delay: 2s to delay the animation 2 seconds, animation-iteration-count: infinite for an infinite loop, 
+                animation-direction: alternate to make sure the animation runs forward then backward in smooth transition, animation-fill-mode: both to extend the rules for both
+                forward and backward motions (to help with alternate), and animation-duration: 5s to make sure the animation runs from 0% to 100% in 5 seconds. <br><br>
+                 
+                 
+            </p>
+            
+            <p class="font">
+                Use the JSFiddle to view the HTML, CSS, and Result.
+                <br> *Use Google Chrome for best result 
+                <iframe height="500" src="//jsfiddle.net/acgriffiths/q3f5zpes/2/embedded/result,html,css/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>
+                <br> 
+            
                 
-                <br>This portfolio site will serve as a collection 
-                of the semester's initial understanding topic 
-                definitions and examples.
+        
+                <br>
+                Timestamps: <br>
+                <a href="#">Yay for Animations</a> <br>
+
+                Animation on Personal Site: <br>
+                <a href="http://lenzancestry.capturedarrow.com" title="336">Lenz Ancestry [under construction]</a>
                 
+                <br>
+                Sources:
+                <br>
+                <a href="#" title="w3Schools">'sources, yo.</a>
             </p>
             
         
         
-        
+        <?php include $_SERVER['DOCUMENT_ROOT']. '/modules/footer.php'; ?>
         
     </body>
         
